8d74a3cd6055a2ac07117c198aba4d8242766a34
RubenTello12 <93735005+RubenTello12@users.noreply.github.com>
2023-12-28 16:45:06 -0600

    first commit
    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="py-10 font-bold">
+        <p className="text-center">APV - Administrador de Pacientes de <span className="text-indigo-600"> Vetetinaria</span></p>
+      </footer>
+    )
+  }
+  
+  export default Footer
